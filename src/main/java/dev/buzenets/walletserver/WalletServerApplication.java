39c05269b3ac05053b620f0703e523e9e886c022
Yury Buzenets <buzenets23@gmail.com>
2019-07-09 23:13:52 +0600

    add spring boot skeleton

---
+++ src/main/java/dev/buzenets/walletserver/WalletServerApplication.java
@@ -0,0 +1,13 @@
+package dev.buzenets.walletserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WalletServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WalletServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/buzenets/walletserver/WalletServerApplicationTests.java
@@ -0,0 +1,16 @@
+package dev.buzenets.walletserver;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WalletServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
